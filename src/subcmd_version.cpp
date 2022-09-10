3f1964e03c89e0c7aa4fe5e4ffba326a81b6ad76
Antonio SJ Musumeci <trapexit@spawn.link>
2022-09-09 21:44:18 -0400

    Update version output

--- src/subcmd_version.cpp
+++ src/subcmd_version.cpp
@@ -28,8 +28,21 @@ namespace SubCmd
   version()
   {
     fmt::print("3it v{}.{}.{}\n\n"
-               "Written by trapexit <trapexit@spawn.link>\n"
-               "https://github.com/trapexit/3it\n"
+               "https://github.com/trapexit/3it\n\n"
+               "ISC License (ISC)\n\n"
+               "Copyright 2022, Antonio SJ Musumeci <trapexit@spawn.link>\n\n"
+               "Permission to use, copy, modify, and/or distribute this software for\n"
+               "any purpose with or without fee is hereby granted, provided that the\n"
+               "above copyright notice and this permission notice appear in all\n"
+               "copies.\n\n"
+               "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL\n"
+               "WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED\n"
+               "WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE\n"
+               "AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL\n"
+               "DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR\n"
+               "PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER\n"
+               "TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR\n"
+               "PERFORMANCE OF THIS SOFTWARE.\n\n"
                ,
                MAJOR,
                MINOR,
