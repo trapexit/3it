d6c1f34b5be1d8bf0688a2ef0ca35e218d301219
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-06 20:26:31 -0600

    subcmd_dump_packed_instructions.cpp

--- src/subcmd_dump_packed_instructions.cpp
+++ src/subcmd_dump_packed_instructions.cpp
@@ -123,9 +123,10 @@ namespace l
             break;
           case PACK_EOL:
             line_size += size;
-            fmt::print("e: size={}; aligned={};\n",
+            fmt::print("e: size={}; aligned={} {};\n",
                        DATA_PACKET_DATA_TYPE_SIZE,
-                       on_32bit_boundary);
+                       on_32bit_boundary,
+                       boundry);
 
             break;
           }
