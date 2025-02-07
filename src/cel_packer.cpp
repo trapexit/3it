89db7709c9bb7da6acc7cba9919bfdeb851f467d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-07 15:04:27 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -552,7 +552,7 @@ api_to_bytevec2(const Bitmap              &b_,
 
         bs.zero_till_32bit_boundary();
         if(bs.tell_u32() < 2)
-          bs.write(BITS_PER_WORD);
+          bs.write(BITS_PER_WORD,0);
       }
 
       pdat.emplace_back(row_pdat);
