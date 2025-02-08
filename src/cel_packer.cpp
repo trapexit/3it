ed8493c8cab4131562e25f7f7c3ccb7dbb722640
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-08 13:17:08 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -177,8 +177,8 @@ matched_bits(const u32 *a_,
   BitStreamReader bsa;
   BitStreamReader bsb;
 
-  bsa.reset((const u8*)a_,a_len_);
-  bsb.reset((const u8*)b_,b_len_);  
+  bsa.reset((const u8*)a_,a_len_ * BYTES_PER_WORD);
+  bsb.reset((const u8*)b_,b_len_ * BYTES_PER_WORD);  
   
   return false;
 }
