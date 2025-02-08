85151211fa06e516080f641e8ed354dcaef16ffb
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-08 14:36:23 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -747,5 +747,6 @@ CelPacker::pack(const Bitmap            &b_,
   pass6_remove_trailing_eol(api);
 
   //  api_to_bytevec(b_,api,pc_,pdat_);
-  api_to_bytevec2(b_,api,pc_,pdat_);
+  // api_to_bytevec2(b_,api,pc_,pdat_);
+  api_to_bytevec3(b_,api,pc_,pdat_);  
 };
