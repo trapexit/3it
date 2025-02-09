29980ee8e57c3bd9a5cf6479fc385c90ddb40582
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-08 18:10:28 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -699,11 +699,8 @@ api_to_bytevec3(const Bitmap              &b_,
 
   for(size_t i = 0; i < rows_pdat.size() - 1; i++)
   {
-
-
-    
-    BitStream &a = rows_pdat[0];
-    BitStream &b = rows_pdat[1];
+    BitStream &a = rows_pdat[i+0];
+    BitStream &b = rows_pdat[i+1];
     int trailing_bits;
     int overlap;
 
