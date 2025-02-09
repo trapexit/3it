fddccda8df1e171dc5b5dc536ce29b61d0852931
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-09 16:20:00 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -803,6 +803,10 @@ pass8_trim_overlap(BitStreamVec &rows_)
       BitStream &b = rows_[i+1];
 
       trailing_bits = (a.size_bits() & (BITS_PER_WORD -1));
+      if(trailing_bits == 0)
+        continue;
+
+      
     }
 }
 
