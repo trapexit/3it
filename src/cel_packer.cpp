1811f32b7bdb78d7bd7986d063cc1ec5dc80e269
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-08 19:34:04 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -627,9 +627,6 @@ api_to_bytevec3(const Bitmap              &b_,
                 ByteVec                   &pdat_)
 {
   std::vector<BitStream> rows_pdat;
-  std::vector<bool> has_eol;
-  std::vector<u8> leading_zeros;
-  std::vector<u8> trailing_zeros;
   u64 offset_width;
 
   offset_width = ::calc_offset_width(pc_.bpp());
