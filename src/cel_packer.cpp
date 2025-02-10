20e62d82bed56b900aae5d35d2826266fba86824
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-09 18:22:58 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -668,7 +668,9 @@ pass7_api_to_bitstreams(const AbstractPackedImage &api_,
       // requires minus 2 words for the length / offset meaning a
       // minimum of 2 words in the CEL data.
       fmt::print("{} {}",
+                 row.size_bits(),
                  row.size_bytes(),
+                 row.size_u32(),
                  row.on_64bit_boundary());
       if(row.size_u32() < 2)
         row.zero_till_64bit_boundary();        
