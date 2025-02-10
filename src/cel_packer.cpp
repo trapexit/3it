3c1247a16b6c49340351e41e40e889243cd2c563
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-09 23:44:02 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -827,7 +827,7 @@ pass9_bsvec_to_bytevec(const BitStreamVec &rows_,
   pdat_.clear();
   for(const auto &row : rows_)
     {
-      // Needs to be word aligned.
+      // Needs to be word aligned and min 2 words.
       if(row.size_bits() < (2 * BITS_PER_WORD))
         row.zero_till_64bit_boundary();
       else
