de10a520a59630a467e3ad0d182ffe1a8425cbf9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-09 23:59:45 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -846,7 +846,7 @@ CelPacker::pack(const Bitmap            &b_,
   pass5_remove_trailing_transparents(api);
   pass6_remove_trailing_eol(api);
   pass7_api_to_bitstreams(api,rows);
-  pass8_trim_overlap(api,rows);
+  //  pass8_trim_overlap(api,rows);
   pass9_bsvec_to_bytevec(api,rows,pdat_);
 
   //api_to_bytevec(b_,api,pc_,pdat_);
