9061a7cb8892f00da4a980bdac4804d2d002e542
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-10 00:12:40 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -807,6 +807,7 @@ pass8_trim_overlap(const AbstractPackedImage &api_,
     }
 }
 
+static
 void
 pass9_pad_rows(const int     offset_width_,
                BitStreamVec &rows_)
@@ -827,6 +828,7 @@ pass9_pad_rows(const int     offset_width_,
     }
 }
 
+static
 void
 pass10_bsvec_to_bytevec(const BitStreamVec &rows_,
                         ByteVec            &pdat_)
