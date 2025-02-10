af0e908cd47bf925a0f10db7c220e5ff1b76042c
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-09 23:01:51 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -795,6 +795,13 @@ pass8_trim_overlap(const AbstractPackedImage &api_,
           continue;
         }
 
+      rows_[i].zero_till_32bit_boundary();
+      rows_[i].write(0,
+                     api_.offset_width,
+                     (rows_[i].size_u32() - 2));
+
+      return;
+
       bool overlap;      
       int trailing_bits;
       BitStream &a = rows_[i+0];
