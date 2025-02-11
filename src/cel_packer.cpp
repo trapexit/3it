dbe83cdad1136c034e7d9d7ea2928ecc8161da3f
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-10 23:40:31 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -452,9 +452,6 @@ pass7_api_to_bitstreams(const AbstractPackedImage &api_,
         row.write(0,
                   api_.offset_width,
                   (word_offset - 2));
-        fmt::println("{} {}",
-                     row.tell_32bits(),
-                     word_offset);
       }
     }
 }
