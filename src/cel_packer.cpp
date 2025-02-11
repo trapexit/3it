d4a1a8f3d31be01c8cbc2aaa6125a3c43b5218e8
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-10 23:39:17 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -449,7 +449,6 @@ pass7_api_to_bitstreams(const AbstractPackedImage &api_,
         int word_offset;
 
         word_offset = std::max((uint64_t)2,row.tell_32bits());
-        row.set_size_32bits(word_offset);
         row.write(0,
                   api_.offset_width,
                   (word_offset - 2));
