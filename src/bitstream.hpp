4c1a93881e05a692da7be9e7e6017e705b06c487
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-10 21:44:22 -0600

    bitstream.hpp

--- src/bitstream.hpp
+++ src/bitstream.hpp
@@ -471,11 +471,7 @@ public:
   std::vector<u8>::iterator
   idx_end()
   {
-    int idx;
-
-    idx = ((_idx + 7) / 8);
-    
-    return std::advance(_data.begin(),idx);
+    return _data.begin() + ((_idx + 7) / 8);
   }
 
   std::vector<u8>::iterator
