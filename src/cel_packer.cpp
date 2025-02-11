2a868cf3f43a12b408cd97e337eae73449a0a995
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-10 21:52:14 -0600

    cel_packer.cpp

--- src/cel_packer.cpp
+++ src/cel_packer.cpp
@@ -447,10 +447,8 @@ pass7_api_to_bitstreams(const AbstractPackedImage &api_,
       // Needs to be done in prep for overlap pass
       {
         int word_offset;
-        
-        word_offset = row.size_u32();
-        if(word_offset < 2)
-          word_offset = 2;
+
+        word_offset = std::max(2,row.size_u32());
         row.write(0,
                   api_.offset_width,
                   (word_offset - 2));
