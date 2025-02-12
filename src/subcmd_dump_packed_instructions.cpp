facaa645e206b905b3ef683afd096c528dc3f17c
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-11 20:33:31 -0600

    subcmd_dump_packed_instructions.cpp

--- src/subcmd_dump_packed_instructions.cpp
+++ src/subcmd_dump_packed_instructions.cpp
@@ -125,11 +125,11 @@ namespace l
           }
       } while(type != PACK_EOL && pixels_read < width);
 
-    fmt::print("row={} end; line_size={}; pixels={}; leftover={};\n\n",
-               row_,
-               line_size,
-               pixels_read,
-               bs_.bits_to_32bit_boundary());
+    fmt::println("row={} end; line_size={}; pixels={}; leftover={};\n",
+                 row_,
+                 line_size,
+                 pixels_read,
+                 bs_.bits_to_32bit_boundary());
   }
 
   void
