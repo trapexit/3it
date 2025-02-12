a2afe0bbe674ab6c3d23395dafa5f20675bb0e5e
Antonio SJ Musumeci <trapexit@spawn.link>
2025-02-11 19:56:40 -0600

    subcmd_dump_packed_instructions.cpp

--- src/subcmd_dump_packed_instructions.cpp
+++ src/subcmd_dump_packed_instructions.cpp
@@ -196,8 +196,7 @@ namespace l
                 u32 x = bs.read(BITS_PER_BYTE * 4);
                 fmt::print("{:08X} ",x);
               }
-            fmt::println("");
-            fmt::println(""row_offset,
+            fmt::println("\noffset: {}",row_offset);
             
             unpack_row(row,bs,ccc);
 
